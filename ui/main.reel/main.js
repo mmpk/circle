4c1ff3be4de2c7d6b2fda852c5ab3edd4c2c0a3d
mmpk <noreply>
2014-08-27 18:34:21 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -19,7 +19,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             var flow = this.templateObjects.flow;
            
             flow.addBeforePathChangeListener("scroll", function(v){
-                console.log("SCROLL: " + v)                                
+                console.log("SCROLL: " + Math.round(v))                                
             });
         }
     }                               
