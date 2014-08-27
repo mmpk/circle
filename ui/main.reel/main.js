eef41e7c8140875faf7abb07e31c0960674181f0
mmpk <noreply>
2014-08-27 18:31:38 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -16,8 +16,8 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     },
     templateDidLoad: {
         value: function() {
-            /*
             var flow = this.templateObjects.flow;
+            /*
             flow.addBeforePathChangeListener("scroll", funtion(v){
                 console.log("SCROLL: "+v)                                
             })*/
