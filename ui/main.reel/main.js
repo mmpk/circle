a5a6a4c60fe4af606df6b352856954b5a0800c5a
mmpk <noreply>
2014-09-05 13:33:20 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -33,7 +33,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         }
     },
 
-    handleMousehover: {
+    handleMouseover: {
         value: function () {
             console.log("HOVER CAPTURE")
         }
