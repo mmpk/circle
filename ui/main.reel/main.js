7f23c53d7f5a280b53ba4d2416dcb4333bc5b7b2
mmpk <noreply>
2014-09-17 20:19:17 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -27,7 +27,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
                 r.content.push(   r.content[t] )
                 if(r.content.length > 12){
                     r.content.splice(0,5)   
-                    flow.scroll = 0
+                    flow.scroll = 1; t=0
                 }
                   console.log("LEN: " + r.content.length)  
                 }
